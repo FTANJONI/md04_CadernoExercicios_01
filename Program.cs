66fc47c00513ec6b5c61e921edf0cb448ac794cc
Fabricio Tanjoni <fabriciotanjoni@gmail.com>
2022-08-26 22:32:28 -0300

    Caderno de exercícios aula 4 exercício 1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace md04_CadernoExercicios_01
+{
+    class Program
+    {
+        /*
+         1. Construa um Algoritmo que, para um grupo de 10 valores inteiros, determine:
+            a) A soma dos números positivos;
+            b) A quantidade de valores negativos;
+            -
+         */
+        static void Main(string[] args)
+        {
+            int n = -4, nrNegativos = 1, somaPositivos = 0;
+
+            while (n <= 5)
+            {
+                if (n < 0)
+                {
+                    nrNegativos++;
+                }
+                else
+                {
+                    somaPositivos = somaPositivos + n;
+                }
+
+                n++;
+            }
+
+            Console.WriteLine("\nO somatório de todos os números positivos foi: " + somaPositivos.ToString());
+            Console.WriteLine("A quantidade de números negativos foi: " + nrNegativos.ToString());
+
+        }
+    }
+}
